5b35ad60d8aea29cb79e5364869fa451fb586a6c
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2024-06-10 16:39:33 -0500

    W01 | Prep 03

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int maxNumber = 10;
+        int randomNumber = randomGenerator.Next(1, maxNumber + 1);
+        int guess = 0;
+        Console.WriteLine($"Guess a number between 1 and {maxNumber}.");
+        while (guess != randomNumber)
+        {
+            if (int.TryParse(Console.ReadLine(), out guess))
+            {
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Invalid Input. Please enter a number between 1 and {maxNumber}.");
+            }
+        }
+        Console.WriteLine("You Guessed it!");
     }
 }
