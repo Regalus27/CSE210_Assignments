19543c0b1bbb3e7347ac79890a348fbea95e09dc
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2024-06-10 19:17:46 -0500

    W01 | Prep 04

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = -1;
+        List<int> userNumbers = new List<int>();
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                userNumbers.Add(userNumber);
+            }
+        }
+
+        int sum = userNumbers.Sum();
+        double average = userNumbers.Average();
+        int largest = userNumbers.Max();
+        // This should really be split into 2 lines / functions
+        int smallest = userNumbers.FindAll(
+            delegate(int num)
+            {
+                return num > 0;
+            }
+        ).Min();
+        userNumbers.Sort();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+        foreach (int num in userNumbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
